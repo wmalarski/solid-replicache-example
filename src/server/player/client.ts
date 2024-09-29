df58b9aa6f17288eb7b9af7f8e46c013337db296
Wojciech Malarski <w.malarski@gmail.com>
2024-09-29 17:13:21 +0200

    renames rpc to server

--- src/server/player/client.ts
+++ src/server/player/client.ts
@@ -1,6 +1,6 @@
 import { cache } from "@solidjs/router";
 
 import { PLAYER_CACHE_KEY } from "./const";
-import { getPlayerIdServerAction } from "./rpc";
+import { getPlayerIdServerAction } from "./server";
 
 export const getPlayerLoader = cache(getPlayerIdServerAction, PLAYER_CACHE_KEY);
