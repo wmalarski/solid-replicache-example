c11f8f4bf6b9aebc8e2df06778ea4d825202baec
Wojciech Malarski <w.malarski@gmail.com>
2024-10-04 16:47:50 +0200

    moves lakes code to utils

--- src/components/modules/board/Board.tsx
+++ src/components/modules/board/Board.tsx
@@ -8,6 +8,7 @@ import type { GameCell } from "~/server/cells/types";
 import { getGameKey } from "~/server/replicache/utils";
 import { Grid, Stack } from "~/styled-system/jsx";
 import { BoardCell } from "./board-cell";
+import { getCellInfos } from "./utils";
 
 type BoardProps = {
 	playerId: string;
@@ -36,13 +37,6 @@ type BoardCellsProps = {
 	code: string;
 };
 
-type CellInfo = {
-	neighbors: number[];
-	crossNeighbors: number[];
-	count: number;
-	lake?: number[];
-};
-
 const BoardCells: Component<BoardCellsProps> = (props) => {
 	const game = createSubscription(async (tx) => {
 		return tx
@@ -56,99 +50,7 @@ const BoardCells: Component<BoardCellsProps> = (props) => {
 		const rows = Math.floor(props.code.length / props.width);
 		const cellCodes = props.code.split("");
 
-		const cellInfos = new Map<number, CellInfo>();
-		const minePositions = new Set<number>();
-		const positionsWithZero = new Array<number>();
-
-		cellCodes.forEach((cellCode, index) => {
-			if (cellCode === "X") {
-				minePositions.add(index);
-			}
-		});
-
-		cellCodes.forEach((_cellCode, index) => {
-			const cellX = index % columns;
-			const cellY = Math.floor(index / columns);
-
-			const fromX = Math.max(0, cellX - 1);
-			const toX = Math.min(columns - 1, cellX + 1);
-			const fromY = Math.max(0, cellY - 1);
-			const toY = Math.min(rows - 1, cellY + 1);
-
-			const positions: number[] = [];
-			const crossPositions: number[] = [];
-
-			for (let x = fromX; x <= toX; x++) {
-				for (let y = fromY; y <= toY; y++) {
-					const position = y * columns + x;
-					if (position !== index) {
-						positions.push(position);
-						if (x === cellX || y === cellY) {
-							crossPositions.push(position);
-						}
-					}
-				}
-			}
-
-			const count = positions.filter((position) =>
-				minePositions.has(position),
-			).length;
-
-			if (count < 1) {
-				positionsWithZero.push(index);
-			}
-
-			cellInfos.set(index, {
-				neighbors: positions,
-				crossNeighbors: crossPositions,
-				count,
-			});
-		});
-
-		positionsWithZero.forEach((position) => {
-			if (cellInfos.get(position)?.lake) {
-				return;
-			}
-
-			const lake = new Array<number>();
-			const queue = [position];
-
-			while (queue.length > 0) {
-				const index = queue.pop();
-
-				if (index === undefined) {
-					continue;
-				}
-
-				lake.push(index);
-
-				const info = cellInfos.get(index);
-
-				if (!info) {
-					continue;
-				}
-
-				cellInfos.set(index, { ...info, lake });
-				const zeroNeighbors = info.crossNeighbors.filter(
-					(neighbor) =>
-						positionsWithZero.includes(neighbor) &&
-						!lake.includes(neighbor) &&
-						!queue.includes(neighbor),
-				);
-
-				queue.push(...zeroNeighbors);
-			}
-
-			const all = new Set(
-				lake
-					.flatMap((index) => cellInfos.get(index)?.neighbors ?? [])
-					.filter((index) => !lake.includes(index)),
-			);
-
-			lake.push(...all);
-		});
-
-		return cellInfos;
+		return getCellInfos({ cellCodes, columns, rows });
 	});
 
 	const cellsMap = createMemo(() => {
---
+++ src/components/modules/board/utils.ts
@@ -0,0 +1,147 @@
+type CellInfo = {
+	neighbors: number[];
+	crossNeighbors: number[];
+	count: number;
+	lake?: number[];
+};
+
+const getMinePositions = (cellCodes: string[]) => {
+	const minePositions = new Set<number>();
+
+	cellCodes.forEach((cellCode, index) => {
+		if (cellCode === "X") {
+			minePositions.add(index);
+		}
+	});
+
+	return minePositions;
+};
+
+type GetNeighborsPositionsArgs = {
+	index: number;
+	columns: number;
+	rows: number;
+};
+
+const getNeighborsPositions = ({
+	columns,
+	index,
+	rows,
+}: GetNeighborsPositionsArgs) => {
+	const cellX = index % columns;
+	const cellY = Math.floor(index / columns);
+
+	const fromX = Math.max(0, cellX - 1);
+	const toX = Math.min(columns - 1, cellX + 1);
+	const fromY = Math.max(0, cellY - 1);
+	const toY = Math.min(rows - 1, cellY + 1);
+
+	const positions: number[] = [];
+	const crossPositions: number[] = [];
+
+	for (let x = fromX; x <= toX; x++) {
+		for (let y = fromY; y <= toY; y++) {
+			const position = y * columns + x;
+			if (position !== index) {
+				positions.push(position);
+				if (x === cellX || y === cellY) {
+					crossPositions.push(position);
+				}
+			}
+		}
+	}
+
+	return { positions, crossPositions };
+};
+
+type GetCellInfosArgs = {
+	cellCodes: string[];
+	columns: number;
+	rows: number;
+};
+
+export const getCellInfos = ({
+	cellCodes,
+	columns,
+	rows,
+}: GetCellInfosArgs) => {
+	const cellInfos = new Map<number, CellInfo>();
+	const minePositions = getMinePositions(cellCodes);
+	const positionsWithZero = new Array<number>();
+
+	cellCodes.forEach((_cellCode, index) => {
+		const { crossPositions, positions } = getNeighborsPositions({
+			index,
+			columns,
+			rows,
+		});
+
+		const count = positions.filter((position) =>
+			minePositions.has(position),
+		).length;
+
+		if (count < 1) {
+			positionsWithZero.push(index);
+		}
+
+		cellInfos.set(index, {
+			neighbors: positions,
+			crossNeighbors: crossPositions,
+			count,
+		});
+	});
+
+	updateLakes({ cellInfos, positionsWithZero });
+
+	return cellInfos;
+};
+
+type UpdateLakesArgs = {
+	positionsWithZero: number[];
+	cellInfos: Map<number, CellInfo>;
+};
+
+const updateLakes = ({ positionsWithZero, cellInfos }: UpdateLakesArgs) => {
+	positionsWithZero.forEach((position) => {
+		if (cellInfos.get(position)?.lake) {
+			return;
+		}
+
+		const lake = new Array<number>();
+		const queue = [position];
+
+		while (queue.length > 0) {
+			const index = queue.pop();
+
+			if (index === undefined) {
+				continue;
+			}
+
+			lake.push(index);
+
+			const info = cellInfos.get(index);
+
+			if (!info) {
+				continue;
+			}
+
+			cellInfos.set(index, { ...info, lake });
+			const zeroNeighbors = info.crossNeighbors.filter(
+				(neighbor) =>
+					positionsWithZero.includes(neighbor) &&
+					!lake.includes(neighbor) &&
+					!queue.includes(neighbor),
+			);
+
+			queue.push(...zeroNeighbors);
+		}
+
+		const all = new Set(
+			lake
+				.flatMap((index) => cellInfos.get(index)?.neighbors ?? [])
+				.filter((index) => !lake.includes(index)),
+		);
+
+		lake.push(...all);
+	});
+};
