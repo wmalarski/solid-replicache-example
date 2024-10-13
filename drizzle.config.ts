02160959103a090ac4550279e191eacc91066b4e
Wojciech Malarski <w.malarski@gmail.com>
2024-10-13 18:41:15 +0200

    adds noNonNullAssertion

--- drizzle.config.ts
+++ drizzle.config.ts
@@ -1,14 +1,16 @@
-import { config } from 'dotenv';
-import { defineConfig } from 'drizzle-kit';
+import { config } from "dotenv";
+import { defineConfig } from "drizzle-kit";
 
-config({ path: '.env.local' });
+config({ path: ".env.local" });
 
 export default defineConfig({
 	schema: "./src/server/db/schema.ts",
 	out: "./drizzle/migrations/",
-  dialect: 'turso',
-  dbCredentials: {
-    url: process.env.TURSO_CONNECTION_URL!,
-    authToken: process.env.TURSO_AUTH_TOKEN!,
-  },
+	dialect: "turso",
+	dbCredentials: {
+		// biome-ignore lint/style/noNonNullAssertion: <explanation>
+		url: process.env.TURSO_CONNECTION_URL!,
+		// biome-ignore lint/style/noNonNullAssertion: <explanation>
+		authToken: process.env.TURSO_AUTH_TOKEN!,
+	},
 });
--- src/server/db/db.ts
+++ src/server/db/db.ts
@@ -1,12 +1,14 @@
 import { createClient } from "@libsql/client";
-import { config } from 'dotenv';
+import { config } from "dotenv";
 import { drizzle } from "drizzle-orm/libsql";
 
-config({ path: '.env.local' });
+config({ path: ".env.local" });
 
 const client = createClient({
-  url: process.env.TURSO_CONNECTION_URL!,
-  authToken: process.env.TURSO_AUTH_TOKEN!,
+	// biome-ignore lint/style/noNonNullAssertion: <explanation>
+	url: process.env.TURSO_CONNECTION_URL!,
+	// biome-ignore lint/style/noNonNullAssertion: <explanation>
+	authToken: process.env.TURSO_AUTH_TOKEN!,
 });
 
 export const db = drizzle(client);
