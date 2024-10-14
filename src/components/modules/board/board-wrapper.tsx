cf162f8fe8350cb968e85d50644330e646ee5644
Wojciech Malarski <w.malarski@gmail.com>
2024-10-14 20:09:44 +0200

    switching to manual on mount

--- src/components/modules/board/board-wrapper.tsx
+++ src/components/modules/board/board-wrapper.tsx
@@ -1,10 +1,25 @@
-import { clientOnly } from "@solidjs/start";
-import type { Component, ComponentProps } from "solid-js";
-
-const Board = clientOnly(() => import("~/components/modules/board/board"));
+import {
+	type Component,
+	type ComponentProps,
+	Show,
+	createSignal,
+	onMount,
+} from "solid-js";
+import Board from "~/components/modules/board/board";
+import { LoadingPlaceholder } from "../common/loading-placeholder";
 
 export const BoardWrapper: Component<ComponentProps<typeof Board>> = (
 	props,
 ) => {
-	return <Board {...props} />;
+	const [isMount, setIsMount] = createSignal(false);
+
+	onMount(() => {
+		setIsMount(true);
+	});
+
+	return (
+		<Show when={isMount()} fallback={<LoadingPlaceholder />}>
+			<Board {...props} />
+		</Show>
+	);
 };
