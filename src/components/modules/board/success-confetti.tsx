44a698bd44bd1422e039a1916315c68db24d3f52
Wojciech Malarski <w.malarski@gmail.com>
2024-10-15 21:14:59 +0200

    refactors confetti a bit

--- src/components/modules/board/success-confetti.tsx
+++ src/components/modules/board/success-confetti.tsx
@@ -8,23 +8,34 @@ const randomInRange = (min: number, max: number) => {
 const DURATION = 15 * 1000;
 const DEFAULTS = { startVelocity: 30, spread: 360, ticks: 60, zIndex: 0 };
 
-const fire = async (particleCount: number, xMin: number, xMax: number) => {
+type FireArgs = {
+	particleCount: number;
+	xMin: number;
+	xMax: number;
+	text?: string;
+};
+
+const fire = async ({ particleCount, xMin, xMax, text }: FireArgs) => {
 	const module = await import("canvas-confetti");
+
+	const shapes = text ? [module.default.shapeFromText({ text })] : undefined;
 
 	module.default({
 		...DEFAULTS,
+		shapes,
 		particleCount,
 		origin: { x: randomInRange(xMin, xMax), y: Math.random() - 0.2 },
 	});
 };
 
-export const SuccessConfetti: Component = () => {
-	const data = useGameData();
+type ConfettiProps = {
+	enabled: boolean;
+	text?: string;
+};
 
+export const Confetti: Component<ConfettiProps> = (props) => {
 	createEffect(() => {
-		const { isSuccess } = data();
-
-		if (!isSuccess()) {
+		if (!props.enabled) {
 			return;
 		}
 
@@ -38,7 +49,10 @@ export const SuccessConfetti: Component = () => {
 			}
 
 			const particleCount = 50 * (timeLeft / DURATION);
-			fire(particleCount, 0.1, 0.3).then(() => fire(particleCount, 0.7, 0.9));
+			const text = props.text;
+			fire({ particleCount, xMin: 0.1, xMax: 0.3, text }).then(() =>
+				fire({ particleCount, xMin: 0.7, xMax: 0.9, text }),
+			);
 		}, 250);
 
 		onCleanup(() => {
@@ -47,4 +61,9 @@ export const SuccessConfetti: Component = () => {
 	});
 
 	return <></>;
+};
+
+export const SuccessConfetti: Component = () => {
+	const data = useGameData();
+	return <Confetti enabled={data().isSuccess()} />;
 };
